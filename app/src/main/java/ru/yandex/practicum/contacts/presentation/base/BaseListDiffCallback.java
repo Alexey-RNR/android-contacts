be10cc0f435a976425246aaa032ff0cd933a5f33
Alexey <mr.alexey.kh@gmail.com>
2024-12-19 00:50:08 +0300

    Контакты 2.0

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -9,18 +9,18 @@ import ru.yandex.practicum.contacts.presentation.main.ContactUi;
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
     @Override
-    public boolean areItemsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.hashCode() == newItem.hashCode();
     }
 
     @Override
-    public boolean areContentsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.equals(newItem);
     }
 
     @Nullable
     @Override
-    public Object getChangePayload(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
         return newItem;
     }
 }
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeAdapter.java
@@ -18,13 +18,14 @@ import java.util.function.Consumer;
 import ru.yandex.practicum.contacts.R;
 import ru.yandex.practicum.contacts.databinding.ItemSortBinding;
 import ru.yandex.practicum.contacts.presentation.base.BaseListDiffCallback;
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeAdapter extends RecyclerView.Adapter<SortTypeAdapter.ViewHolder> {
+public class SortTypeAdapter extends RecyclerView.Adapter<SortTypeAdapter.ViewHolder> implements ListDiffInterface<SortTypeAdapter> {
 
     private final AsyncListDiffer<SortTypeUI> differ = new AsyncListDiffer<>(
             new AdapterListUpdateCallback(this),
-            new AsyncDifferConfig.Builder<>(new BaseListDiffCallback<SortTypeAdapter.ViewHolder>()).build()
+            new AsyncDifferConfig.Builder<>(new BaseListDiffCallback<SortTypeUI>()).build()
     );
 
     private final Consumer<SortTypeUI> clickListener;
@@ -53,6 +54,11 @@ public class SortTypeAdapter extends RecyclerView.Adapter<SortTypeAdapter.ViewHo
 
     public void setItems(List<SortTypeUI> items) {
         differ.submitList(items);
+    }
+
+    @Override
+    public boolean theSameAs(SortTypeAdapter other) {
+        return false;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
